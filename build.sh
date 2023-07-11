8225c624e9dcc0f1f586787a22432a3152236bdc
Johannes Bechberger <johannes.bechberger@sap.com>
2023-07-11 17:38:11 +0200

    Fix on Linux

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname "$0")
 if [ -d "$JAVA_HOME/include/linux" ]; then
-  g++ cpp/libSmallProfiler.cpp -I$JAVA_HOME/include/linux -I$JAVA_HOME/include -o libSmallProfiler.so -std=c++17 -shared
+  g++ cpp/libSmallProfiler.cpp -I$JAVA_HOME/include/linux -I$JAVA_HOME/include -o libSmallProfiler.so -std=c++17 -shared -fPIC
 else
   g++ cpp/libSmallProfiler.cpp -I$JAVA_HOME/include/darwin -I$JAVA_HOME/include -o libSmallProfiler.so -std=c++17 -shared
 fi
