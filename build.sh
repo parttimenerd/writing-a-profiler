606efffdee3f1249b685878ade1221f2fcef15df
Johannes Bechberger <johannes.bechberger@sap.com>
2023-07-13 10:24:47 +0200

    Build with debug symbols

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 cd $(dirname "$0")
 if [ -d "$JAVA_HOME/include/linux" ]; then
-  g++ cpp/libSmallProfiler.cpp -I$JAVA_HOME/include/linux -I$JAVA_HOME/include -o libSmallProfiler.so -std=c++17 -shared -fPIC
+  g++ cpp/libSmallProfiler.cpp -I$JAVA_HOME/include/linux -I$JAVA_HOME/include -o libSmallProfiler.so -std=c++17 -shared -fPIC -g
 else
-  g++ cpp/libSmallProfiler.cpp -I$JAVA_HOME/include/darwin -I$JAVA_HOME/include -o libSmallProfiler.so -std=c++17 -shared
+  g++ cpp/libSmallProfiler.cpp -I$JAVA_HOME/include/darwin -I$JAVA_HOME/include -o libSmallProfiler.so -std=c++17 -shared -g
 fi
 
 (
